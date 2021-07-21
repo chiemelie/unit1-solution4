21fadc093c7f3262b38c5b29a42feb354e225713
Chiemelieobi Okonkwo <chiemelieobiokonkwo@gmail.com>
2021-07-21 08:25:30 +0300

    unit1-solution4

---
+++ unit1-solution4.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+mkdir unit1exercise4
+cd unit1exercise4
+mkdir ausgewitzs
+chgrp dip ausgewitzs
+chmod 2646 ausgewitzs
+mkdir ausgetrause
+chgrp audio ausgetrause
+chmod 025 ausgetrause
+mkdir aushundheit
+chgrp proxy aushundheit
+chmod 040 aushundheit
+mkdir versteht
+chgrp cdrom versteht
+chmod 2156 versteht
+mkdir verklettheit
+chgrp voice verklettheit
+chmod 632 verklettheit
+mkdir enkaesung
+chgrp audio enkaesung
+chmod 2625 enkaesung
+mkdir zersitztest
+chgrp mail zersitztest
+chmod 225 zersitztest
+mkdir angetrause
+chgrp cdrom angetrause
+chmod 2775 angetrause
+mkdir versteht/versinntest
+chgrp uucp versteht/versinntest
+chmod 560 versteht/versinntest
+mkdir angetrause/angekaess
+chgrp voice angetrause/angekaess
+chmod 312 angetrause/angekaess
+mkdir enkaesung/ausgesitzer
+chgrp floppy enkaesung/ausgesitzer
+chmod 736 enkaesung/ausgesitzer
+mkdir angetrause/einkaesung
+chgrp dip angetrause/einkaesung
+chmod 2643 angetrause/einkaesung
+mkdir angetrause/aufgewarfkeit
+chgrp mail angetrause/aufgewarfkeit
+chmod 255 angetrause/aufgewarfkeit
+mkdir angetrause/einkaesung/ankraus
+chgrp proxy angetrause/einkaesung/ankraus
+chmod 2607 angetrause/einkaesung/ankraus
+mkdir angetrause/angekaess/angegehung
+chgrp student angetrause/angekaess/angegehung
+chmod 2703 angetrause/angekaess/angegehung
+mkdir angetrause/aufgewarfkeit/gerabarbse
+chgrp proxy angetrause/aufgewarfkeit/gerabarbse
+chmod 2704 angetrause/aufgewarfkeit/gerabarbse
+mkdir angetrause/angekaess/verwarfheit
+chgrp voice angetrause/angekaess/verwarfheit
+chmod 362 angetrause/angekaess/verwarfheit
+mkdir enkaesung/ausgesitzer/enschmeckst
+chgrp tape enkaesung/ausgesitzer/enschmeckst
+chmod 305 enkaesung/ausgesitzer/enschmeckst
+mkdir versteht/versinntest/gerauchse
+chgrp fax versteht/versinntest/gerauchse
+chmod 071 versteht/versinntest/gerauchse
+mkdir enkaesung/ausgesitzer/antritts
+chgrp proxy enkaesung/ausgesitzer/antritts
+chmod 2617 enkaesung/ausgesitzer/antritts
+cd ..
+tar zcf unit1-solution4.tgz unit1exercise4
