6d13ba5046d499ce800db5eb4a92dcf8971890d3
Chiemelieobi Okonkwo <chiemelieobiokonkwo@gmail.com>
2021-07-21 08:41:28 +0300

    solutions 1 and 2

---
+++ unit1-solution1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+mkdir unit1exercise1
+cd unit1exercise1
+mkdir enrabarbkeit
+chmod 410 enrabarbkeit
+mkdir aufgehundung
+chmod 276 aufgehundung
+mkdir aufgeschmecken
+chmod 521 aufgeschmecken
+mkdir ausgesitzheit
+chmod 305 ausgesitzheit
+mkdir geschmeckse
+chmod 431 geschmeckse
+mkdir vertritter
+chmod 057 vertritter
+mkdir ensitzte
+chmod 664 ensitzte
+mkdir gewarfs
+chmod 052 gewarfs
+mkdir vertritter/belaufen
+chmod 265 vertritter/belaufen
+mkdir vertritter/aufrennte
+chmod 603 vertritter/aufrennte
+mkdir enrabarbkeit/angesinntest
+chmod 373 enrabarbkeit/angesinntest
+mkdir enrabarbkeit/einwitztest
+chmod 174 enrabarbkeit/einwitztest
+mkdir vertritter/aussetzte
+chmod 666 vertritter/aussetzte
+mkdir enrabarbkeit/einwitztest/einwarfheit
+chmod 133 enrabarbkeit/einwitztest/einwarfheit
+mkdir vertritter/aufrennte/gesitzer
+chmod 052 vertritter/aufrennte/gesitzer
+mkdir vertritter/belaufen/einkatzeen
+chmod 360 vertritter/belaufen/einkatzeen
+mkdir enrabarbkeit/einwitztest/zerhunds
+chmod 313 enrabarbkeit/einwitztest/zerhunds
+mkdir vertritter/aufrennte/entrautete
+chmod 341 vertritter/aufrennte/entrautete
+mkdir vertritter/aussetzte/ankraute
+chmod 343 vertritter/aussetzte/ankraute
+mkdir enrabarbkeit/einwitztest/antritttest
+chmod 656 enrabarbkeit/einwitztest/antritttest
+cd ..
+tar zcf unit1-solution1.tgz unit1exercise1
---
+++ unit1-solution2.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+mkdir unit1exercise2
+cd unit1exercise2
+mkdir aufgefahrtest
+chown games:voice aufgefahrtest
+chmod 601 aufgefahrtest
+mkdir ensprachtete
+chown news:audio ensprachtete
+chmod 552 ensprachtete
+mkdir angelaufst
+chown games:voice angelaufst
+chmod 627 angelaufst
+mkdir gehundse
+chown mail:floppy gehundse
+chmod 606 gehundse
+mkdir angerabarbt
+chown proxy:audio angerabarbt
+chmod 055 angerabarbt
+mkdir ansprachte
+chown student:uucp ansprachte
+chmod 036 ansprachte
+mkdir eingehung
+chown proxy:audio eingehung
+chmod 604 eingehung
+mkdir ausrabarber
+chown nobody:student ausrabarber
+chmod 632 ausrabarber
+mkdir ausrabarber/enkatzese
+chown lp:uucp ausrabarber/enkatzese
+chmod 614 ausrabarber/enkatzese
+mkdir ensprachtete/ankletter
+chown news:cdrom ensprachtete/ankletter
+chmod 117 ensprachtete/ankletter
+mkdir angerabarbt/angepflumung
+chown student:fax angerabarbt/angepflumung
+chmod 521 angerabarbt/angepflumung
+mkdir angelaufst/angekaesen
+chown uucp:dip angelaufst/angekaesen
+chmod 730 angelaufst/angekaesen
+mkdir gehundse/einhaltte
+chown lp:student gehundse/einhaltte
+chmod 743 gehundse/einhaltte
+mkdir ensprachtete/ankletter/angekaesheit
+chown news:audio ensprachtete/ankletter/angekaesheit
+chmod 271 ensprachtete/ankletter/angekaesheit
+mkdir angerabarbt/angepflumung/auftrittte
+chown games:cdrom angerabarbt/angepflumung/auftrittte
+chmod 512 angerabarbt/angepflumung/auftrittte
+mkdir ausrabarber/enkatzese/berabarber
+chown nobody:fax ausrabarber/enkatzese/berabarber
+chmod 336 ausrabarber/enkatzese/berabarber
+mkdir gehundse/einhaltte/berennkeit
+chown mail:tape gehundse/einhaltte/berennkeit
+chmod 547 gehundse/einhaltte/berennkeit
+mkdir gehundse/einhaltte/zersitzen
+chown mail:student gehundse/einhaltte/zersitzen
+chmod 367 gehundse/einhaltte/zersitzen
+mkdir angelaufst/angekaesen/verkatzetete
+chown uucp:proxy angelaufst/angekaesen/verkatzetete
+chmod 614 angelaufst/angekaesen/verkatzetete
+mkdir ausrabarber/enkatzese/angegehtest
+chown proxy:cdrom ausrabarber/enkatzese/angegehtest
+chmod 715 ausrabarber/enkatzese/angegehtest
+cd ..
+tar zcf unit1-solution2.tgz unit1exercise2
