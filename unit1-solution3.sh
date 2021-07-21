2f0d1d68f097146466987beadb3a349f18221c9d
Chiemelieobi Okonkwo <chiemelieobiokonkwo@gmail.com>
2021-07-21 08:37:09 +0300

    unit1-solution3

---
+++ unit1-solution3.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+mkdir unit1exercise3
+cd unit1exercise3
+mkdir gekatzeung
+chown news:cdrom gekatzeung
+chmod 4464 gekatzeung
+mkdir zerpflumse
+chown uucp:proxy zerpflumse
+chmod 4206 zerpflumse
+mkdir ausgerennst
+chown mail:mail ausgerennst
+chmod 135 ausgerennst
+mkdir gesprachung
+chown mail:cdrom gesprachung
+chmod 175 gesprachung
+mkdir angeklettt
+chown student:floppy angeklettt
+chmod 4612 angeklettt
+mkdir getrauen
+chown games:cdrom getrauen
+chmod 204 getrauen
+mkdir gesitzung
+chown mail:student gesitzung
+chmod 4012 gesitzung
+mkdir einlaufs
+chown nobody:fax einlaufs
+chmod 4714 einlaufs
+mkdir gesprachung/antrittse
+chown mail:mail gesprachung/antrittse
+chmod 4575 gesprachung/antrittse
+mkdir einlaufs/ausgesetzst
+chown games:tape einlaufs/ausgesetzst
+chmod 4717 einlaufs/ausgesetzst
+mkdir getrauen/angetraus
+chown games:audio getrauen/angetraus
+chmod 426 getrauen/angetraus
+mkdir gekatzeung/enwitzer
+chown games:student gekatzeung/enwitzer
+chmod 630 gekatzeung/enwitzer
+mkdir gekatzeung/verhaltse
+chown nobody:fax gekatzeung/verhaltse
+chmod 4620 gekatzeung/verhaltse
+mkdir getrauen/angetraus/berabarbtest
+chown news:tape getrauen/angetraus/berabarbtest
+chmod 4502 getrauen/angetraus/berabarbtest
+mkdir gesprachung/antrittse/zerstehheit
+chown student:cdrom gesprachung/antrittse/zerstehheit
+chmod 4272 gesprachung/antrittse/zerstehheit
+mkdir einlaufs/ausgesetzst/ausgepflums
+chown proxy:floppy einlaufs/ausgesetzst/ausgepflums
+chmod 4614 einlaufs/ausgesetzst/ausgepflums
+mkdir gekatzeung/verhaltse/aufhundte
+chown uucp:voice gekatzeung/verhaltse/aufhundte
+chmod 733 gekatzeung/verhaltse/aufhundte
+mkdir gekatzeung/enwitzer/verhundt
+chown mail:proxy gekatzeung/enwitzer/verhundt
+chmod 152 gekatzeung/enwitzer/verhundt
+mkdir gekatzeung/enwitzer/bekaesheit
+chown proxy:voice gekatzeung/enwitzer/bekaesheit
+chmod 4564 gekatzeung/enwitzer/bekaesheit
+mkdir getrauen/angetraus/getrautest
+chown games:news getrauen/angetraus/getrautest
+chmod 4533 getrauen/angetraus/getrautest
+cd ..
+tar zcf unit1-solution3.tgz unit1exercise3
